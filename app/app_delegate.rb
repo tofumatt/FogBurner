40b7ebb01b9682f03e1be84057f996161b93a747
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-11-04 13:41:12 -0500

    Use Yosemite Cocoa conventions; add right click

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,4 @@
 class AppDelegate
-  attr_accessor :statusMenu
-
   def applicationDidFinishLaunching(notification)
     @app_name = NSBundle.mainBundle.infoDictionary["CFBundleDisplayName"]
 
@@ -12,60 +10,56 @@ class AppDelegate
     @statusMenu.addItem NSMenuItem.separatorItem
     @statusMenu.addItem createMenuItem("Quit", "terminate")
 
-    image = NSImage.imageNamed "black-outline.png"
+    image = NSImage.imageNamed("black-outline")
     image.setSize(NSMakeSize(20, 20))
+    image.setTemplate(true)
 
     @statusItem = NSStatusBar.systemStatusBar
-                             .statusItemWithLength(NSVariableStatusItemLength)
+                             .statusItemWithLength(NSSquareStatusItemLength)
 
-    @statusItem.setHighlightMode(false)
     @statusItem.setTitle(nil)
-    @statusItem.setImage(image)
+    @statusItem.button.setImage(image)
 
+    @statusItem.button
+               .sendActionOn(NSLeftMouseUpMask|NSRightMouseUpMask)
     @statusItem.setAction(:activate)
   end
 
   def activate
     event = NSApp.currentEvent
-    # If Control or Option key is being held, load the preferences.
+
+    # If Control or Option key is being held, or it's a right-click, load the
+    # menu instead of toggling caffeination.
     if event.modifierFlags & NSControlKeyMask != 0 or
-       event.modifierFlags & NSAlternateKeyMask != 0
+       event.modifierFlags & NSAlternateKeyMask != 0 or
+       event.type == NSRightMouseUp
       openMenu
     else
       toggle
     end
   end
 
+  def caffeinate
+    image = NSImage.imageNamed "black-fill.png"
+    image.setSize(NSMakeSize(20, 20))
+
+    @statusItem.setImage image
+
+    @caffeinate = NSTask.new
+    @caffeinate.setLaunchPath('/usr/bin/caffeinate')
+    @caffeinate.launch
+  end
+
+  def caffeinated?
+    !!@caffeinate
+  end
+
   def createMenuItem(name, action)
     NSMenuItem.alloc.initWithTitle(name, action: action, keyEquivalent: '')
   end
 
-  def openMenu
-    @statusItem.setHighlightMode(true)
-    @statusItem.popUpStatusItemMenu(@statusMenu)
-    @statusItem.setHighlightMode(false)
-  end
-
-  # TODO: Implement this.
-  # def openPreferences
-  #
-  # end
-
-  def terminate
-    turnOffCaffeinate
-    NSApp.performSelector("terminate:", withObject:nil)
-  end
-
-  def toggle
-    if @caffeinate
-      turnOffCaffeinate
-    else
-      turnOnCaffeinate
-    end
-  end
-
-  def turnOffCaffeinate
-    return unless @caffeinate
+  def decaffeinate
+    return unless caffeinated?
 
     image = NSImage.imageNamed "black-outline.png"
     image.setSize(NSMakeSize(20, 20))
@@ -76,14 +70,29 @@ class AppDelegate
     @caffeinate = nil
   end
 
-  def turnOnCaffeinate
-    image = NSImage.imageNamed "black-fill.png"
-    image.setSize(NSMakeSize(20, 20))
+  def openMenu
+    @statusItem.popUpStatusItemMenu(@statusMenu)
+  end
 
-    @statusItem.setImage image
+  # TODO: Implement this.
+  # def openPreferences
+  #
+  # end
 
-    @caffeinate = NSTask.new
-    @caffeinate.setLaunchPath('/usr/bin/caffeinate')
-    @caffeinate.launch
+  def terminate
+    # Make sure to disable the `caffeinate` command first; otherwise it will
+    # keep running, detached from the app, which is bad.
+    decaffeinate()
+
+    # Quit the app.
+    NSApp.performSelector("terminate:", withObject:nil)
+  end
+
+  def toggle
+    if caffeinated?
+      decaffeinate()
+    else
+      caffeinate()
+    end
   end
 end
